aa48bfd17593de3e612c14c4ac0046ee9e5427fd
Wandiyahyel Joseph <best9ja1@gmail.com>
2025-01-27 11:04:31 -0800

    myport

--- wanjos/admin.py
+++ wanjos/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from .models import service, Contact
-
-admin.site.register(service)
-admin.site.register(Contact)
--- wanjos/views.py
+++ wanjos/views.py
@@ -1,40 +1,23 @@
-from django.shortcuts import render,HttpResponse
-from .models import Contact
-from django.http import HttpResponse
-from django.core.mail import send_mail
+from django.shortcuts import render
+from django.core.mail import EmailMessage
+from django.contrib import messages
 from django.conf import settings
 
 def index(request):
-    context = {}
 
     if request.method == 'POST':
         name = request.POST.get("name")
         email = request.POST.get("email")
         message = request.POST.get("message")
-        service = request.POST.get("service")
         subject = "Freelance Job Alert"
-
-        if subject and name and email and message and service:
-            try:
-                send_mail(subject, message,name,service, settings.EMAIL_HOST_USER, [email])
-                context['result'] = 'Email sent successfully'
-            except Exception as e:
-                context['result'] = f'Error sending email: {e}'
-        else:
-            context['result'] = 'All fields are required'
-    
-    return render(request, "index.html", context)
-"""
-def index(request):
-    if request.method =="POST":
-        name = request.POST.get("name")
-        email = request.POST.get("email")
-        message = request.POST.get("message")
-        service = request.POST.get("service")
-        savetodatabase = Contact.objects.create(name =name, email = email, message=message,service_id = service)
-        savetodatabase.save()
-        return HttpResponse("Message sent succesfully!, I will respond to you Through Email")
         
+        sending = EmailMessage(
+        subject = subject,
+        body = f"{name}\n{ message,}",
+        from_email = email,
+        to = [settings.EMAIL_HOST_USER],
+        )
+        sending.send(fail_silently=False,)
+        messages.success(request, "Message sent successfullfuly")     
+    return render(request, "index.html")
 
-    return render (request, "index.html")
-"""
